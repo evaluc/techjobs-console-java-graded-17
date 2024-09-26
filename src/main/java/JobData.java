5d958b047cdbc9a92206fc3a015fe14d956dadcf
evaluc <ealuckow@gmail.com>
2024-09-26 13:47:45 -0500

    Implement Task 2 findByValue & pass test

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -61,7 +61,7 @@ public class JobData {
      * with "Enterprise Holdings, Inc".
      *
      * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param value Value of the field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -95,7 +95,23 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            //String aValue =  //figure out what this is;
+            if (jobs.contains(row)) {
+                continue;
+            }
+
+            for (String rowValue : row.values()) {
+                if (rowValue.contains(value)) {
+                    jobs.add(row);
+                    //do I need to add break; here?
+                }
+            }
+        }
+
+        return jobs;
     }
 
     /**
